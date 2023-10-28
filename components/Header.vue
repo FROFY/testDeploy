9a0c0e050c865b062633f362da27b7d9f2904ac6
FROFY <karos-76@yandex.ru>
2023-10-29 01:26:09 +0300

    init

---
+++ components/Header.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+// Columns
+const columns = [{
+    key: 'id',
+    label: 'Номер',
+    sortable: true
+}, {
+    key: 'title',
+    label: 'Название',
+    sortable: true
+}, {
+    key: 'completed',
+    label: 'Статус',
+    sortable: true
+}, {
+    key: 'actions',
+    label: 'Дополнительно',
+    sortable: false
+}]
+
+// const users = fetch('http://127.0.0.1:8000/')
+
+// console.log(users)
+</script>
+
+<template>
+    <UCard class="w-full">
+        <UBadge>Press -></UBadge>
+        <UTextarea></UTextarea>
+        <UButton>Press me</UButton>
+    </UCard>
+</template>
